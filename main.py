e7e2613908d79d58b0e23902d340ad3805c7b92f
dnaszhao <dnaszhao2966@gmail.com>
2025-06-23 16:16:53 +0800

    版本检测脚本

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# 这是一个示例 Python 脚本。
+if __name__ == "__main__":
+    from ultralytics import YOLO
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+    # # 从配置文件创建新的模型
+    # model = YOLO("yolov8n.yaml")
+    #
+    # # 训练模型
+    # model.train(data="coco8.yaml", epochs=3)
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    # 第二步推理
+    model = YOLO("best.pt")
+    results = model.predict(show=True)
